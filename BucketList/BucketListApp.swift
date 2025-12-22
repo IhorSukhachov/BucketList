a0e274b7d6ad9456e46f1997adaf0c113bc6fa6f
Ihor Sukhachov <i.sukhachov@gmail.com>
2025-12-22 16:27:11 +0200

    Initial Commit

---
+++ BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Ihor Sukhachov on 22.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
