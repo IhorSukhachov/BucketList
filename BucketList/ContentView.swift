ea2b1d1943e617f2f9e3ecf65f395ead7dea4b50
Ihor Sukhachov <i.sukhachov@gmail.com>
2025-12-22 17:19:47 +0200

    switching views

--- BucketList/ContentView.swift
+++ BucketList/ContentView.swift
@@ -7,40 +7,49 @@
 
 import SwiftUI
 
-struct User: Comparable, Identifiable {
-    let id = UUID()
-    let firstName: String
-    let lastName: String
-    
-    static func <(lhs: User, rhs: User) -> Bool {
-        lhs.lastName < rhs.lastName
+struct LoadingView: View {
+    var body: some View {
+        Text("Loading...")
     }
 }
 
+struct SuccessView: View {
+    var body: some View {
+        Text("Success!")
+    }
+}
+
+struct FailedView: View {
+    var body: some View {
+        Text("Failed.")
+    }
+}
+
+
 struct ContentView: View {
-    let users = [
-        User(firstName: "Ihor", lastName: "Sukhachov"),
-        User(firstName: "Andrii", lastName: "Kozlov"),
-        User(firstName: "Oleg", lastName: "Shevchenko"),
-    ].sorted()
+    enum LoadingState {
+        case loading, success, failed
+    }
+    
+    @State private var loadingState: LoadingState = .loading
     
     var body: some View {
         VStack {
-            List(users) {user in
-                Text("\(user.firstName) \(user.lastName)")
-            }
-            Button("Read and write") {
-                let data = Data("Hello, world!".utf8)
-                let url = URL.documentsDirectory.appending(path: "message.txt")
-                
-                do {
-                    try data.write(to: url, options: [.atomic, .completeFileProtection])
-                    let input = try String(contentsOf: url, encoding: .utf8)
-                    print(input)
-                    
-                } catch {
-                    print("error: \(error.localizedDescription)")
-                }
+//            if loadingState == .loading {
+//                LoadingView()
+//            } else if loadingState == .success {
+//                SuccessView()
+//            } else if loadingState == .failed {
+//                FailedView()
+//            }
+            
+            switch loadingState {
+            case .loading:
+                LoadingView()
+            case .success:
+                SuccessView()
+            case .failed:
+                FailedView()
             }
         }
         .padding()
