c183b8aee68d36cc68e4a0b60ed173632ab41530
Ihor Sukhachov <i.sukhachov@gmail.com>
2025-12-23 15:17:35 +0200

    importing map

--- BucketList/ContentView.swift
+++ BucketList/ContentView.swift
@@ -4,53 +4,54 @@
 //
 //  Created by Ihor Sukhachov on 22.12.2025.
 //
-
+import MapKit
 import SwiftUI
 
-struct LoadingView: View {
-    var body: some View {
-        Text("Loading...")
-    }
-}
-
-struct SuccessView: View {
-    var body: some View {
-        Text("Success!")
-    }
-}
-
-struct FailedView: View {
-    var body: some View {
-        Text("Failed.")
-    }
-}
+//struct LoadingView: View {
+//    var body: some View {
+//        Text("Loading...")
+//    }
+//}
+//
+//struct SuccessView: View {
+//    var body: some View {
+//        Text("Success!")
+//    }
+//}
+//
+//struct FailedView: View {
+//    var body: some View {
+//        Text("Failed.")
+//    }
+//}
 
 
 struct ContentView: View {
-    enum LoadingState {
-        case loading, success, failed
-    }
-    
-    @State private var loadingState: LoadingState = .loading
+//    enum LoadingState {
+//        case loading, success, failed
+//    }
+//    
+//    @State private var loadingState: LoadingState = .loading
     
     var body: some View {
         VStack {
-//            if loadingState == .loading {
+            Map()
+////            if loadingState == .loading {
+////                LoadingView()
+////            } else if loadingState == .success {
+////                SuccessView()
+////            } else if loadingState == .failed {
+////                FailedView()
+////            }
+//            
+//            switch loadingState {
+//            case .loading:
 //                LoadingView()
-//            } else if loadingState == .success {
+//            case .success:
 //                SuccessView()
-//            } else if loadingState == .failed {
+//            case .failed:
 //                FailedView()
 //            }
-            
-            switch loadingState {
-            case .loading:
-                LoadingView()
-            case .success:
-                SuccessView()
-            case .failed:
-                FailedView()
-            }
         }
         .padding()
     }
