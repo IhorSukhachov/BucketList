8de8e5000dc6c3c3f5bab4891053e776da55afc0
Ihor Sukhachov <i.sukhachov@gmail.com>
2025-12-24 22:41:04 +0200

    clearing code

--- BucketList/ContentView.swift
+++ BucketList/ContentView.swift
@@ -4,43 +4,15 @@
 //
 //  Created by Ihor Sukhachov on 22.12.2025.
 //
-import LocalAuthentication
+
+import MapKit
 import SwiftUI
 
 
 struct ContentView: View {
     
-    @State private var isUnlock: Bool = false
-    
     var body: some View {
-        VStack {
-            if isUnlock {
-                Text("is unlocked")
-            }
-            else {
-                Text("Locked")
-            }
-        }
-        .onAppear(perform: authenticate)
-    }
-    func authenticate() {
-         let context = LAContext()
-        var error: NSError?
-        
-        if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
-            let reason = "We need to unlock your data"
-            context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) { success, authentificationError in
-                if success {
-                    isUnlock = true
-                } else {
-                     //pronlem
-                }
-                 
-            }
-        }
-        else {
-            //no bio
-        }
+
     }
 }
 
