048f9193cc1dfd197f13005b6dc1eac43c27f342
Ihor Sukhachov <i.sukhachov@gmail.com>
2025-12-24 23:00:08 +0200

    filling location properties

--- BucketList/Location.swift
+++ BucketList/Location.swift
@@ -6,3 +6,13 @@
 //
 
 import Foundation
+
+struct Location: Codable, Equatable, Identifiable {
+    let id: UUID
+    var name: String
+    var description: String
+    
+    var latitude: Double
+    var longitude: Double
+    
+}
