a16a40b3c35e9cc8e9f7e6766631f424e7e009da
Ihor Sukhachov <i.sukhachov@gmail.com>
2025-12-24 22:51:19 +0200

    displaying coordinates on the SCREEN (not MAP)

--- BucketList/ContentView.swift
+++ BucketList/ContentView.swift
@@ -21,8 +21,8 @@ struct ContentView: View {
     var body: some View {
         Map(initialPosition: startPosition)
             .mapStyle(.hybrid)
-            .onTapGesture {
-                <#code#>
+            .onTapGesture { position in
+                 print("Tapped at \(position)")
             }
     }
 }
