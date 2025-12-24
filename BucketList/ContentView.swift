27ee4410c9bc0f34cb7ef81485c8e741cc19d2ba
Ihor Sukhachov <i.sukhachov@gmail.com>
2025-12-24 23:04:48 +0200

    adding locations to the array

--- BucketList/ContentView.swift
+++ BucketList/ContentView.swift
@@ -18,13 +18,16 @@ struct ContentView: View {
         )
     )
     
+    @State private var locations = [Location]()
+    
     var body: some View {
         MapReader { proxy in
             Map(initialPosition: startPosition)
                 .mapStyle(.hybrid)
                 .onTapGesture { position in
                     if let coordinate = proxy.convert(position, from: .local) {
-                        print("Tapped at \(coordinate)")
+                        let newLocation = Location(id: UUID(), name: "New Location", description: "", latitude: coordinate.latitude, longitude: coordinate.longitude)
+                        locations.append(newLocation)
                     }
                     
                 }
