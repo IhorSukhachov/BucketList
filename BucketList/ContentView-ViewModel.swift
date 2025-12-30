f3088cd0ad6135c556a3e6d41db78168d4e20368
Ihor Sukhachov <i.sukhachov@gmail.com>
2025-12-30 14:22:35 +0200

    add save path to content view viewmode;

--- BucketList/ContentView-ViewModel.swift
+++ BucketList/ContentView-ViewModel.swift
@@ -15,6 +15,17 @@ extension ContentView {
         private(set) var locations = [Location]()
         var selectedPlace : Location?
         
+        let savePath = URL.documentsDirectory.appending(path: "SavedPlaces")
+        
+        init() {
+            do {
+                let data = try Data(contentsOf: savePath)
+                locations = try JSONDecoder().decode([Location].self, from: data)
+            } catch {
+                locations = []
+            }
+        }
+        
         func addLocation(at point: CLLocationCoordinate2D) {
             let newLocation = Location(id: UUID(), name: "New Location", description: "", latitude: point.latitude, longitude: point.longitude)
             locations.append(newLocation)
