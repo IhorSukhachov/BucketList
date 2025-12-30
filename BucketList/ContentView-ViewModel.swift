0e1ad742bf0b91a8d526fc186bdc321dfb24ec40
Ihor Sukhachov <i.sukhachov@gmail.com>
2025-12-30 14:59:59 +0200

    adding authentification method to model view

--- BucketList/ContentView-ViewModel.swift
+++ BucketList/ContentView-ViewModel.swift
@@ -7,6 +7,7 @@
 
 import CoreLocation
 import Foundation
+import LocalAuthentication
 import MapKit
 
 extension ContentView {
@@ -14,6 +15,7 @@ extension ContentView {
     class ViewModel {
         private(set) var locations: [Location]
         var selectedPlace : Location?
+        var isUnlocked: Bool = false
         
         let savePath = URL.documentsDirectory.appending(path: "SavedPlaces")
         
@@ -46,6 +48,27 @@ extension ContentView {
             if let index = locations.firstIndex(of: selectedPlace) {
                 locations[index] = location
                 save()
+            }
+        }
+        
+        func authencticate() {
+            let context = LAContext()
+            var error: NSError?
+            
+            if context.canEvaluatePolicy(LAPolicy.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+                let reason = "Please authenticate to unlock places"
+                
+                context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) {success, authenticationError in
+                    if success {
+                        self.isUnlocked = true
+                    }
+                    else {
+                        //error
+                    }
+                }
+            }
+            else {
+                //no bio
             }
         }
     }
